b52f9fd86eb2629ee5b64b44daf814874f0ebeba
Narendra <59531235+garuda-naru@users.noreply.github.com>
2023-07-27 21:41:35 +0530

    Worked on Links on NavBar

--- curdApp/src/app/app.component.ts
+++ curdApp/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements DoCheck{
+  isMenurequired = false;
+  constructor(private router:Router){
+
+  }
+  ngDoCheck(): void {
+    let currentUrl = this.router.url;
+    if (currentUrl == '/login' || currentUrl == '/register') {
+      this.isMenurequired = false;
+    } else {
+      this.isMenurequired = true;
+    }
+  }
   title = 'curdApp';
 }
